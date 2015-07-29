d2ab440a23335178bb39d3d68103342c7fe635b7
rfaldetta <ross.faldetta@gmail.com>
2015-07-29 01:05:14 -0500

    Completes R and D

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,20 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @list_of_photos = Photo.all
+    @id = params["id"]
+
+    render("details.html.erb")
+  end
+
+  def destroy
+
+    @id = params["id"]
+    p = Photo.find(@id)
+    p.desroy
+
+    render("deleted.html.erb")
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
+
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
 
 end
